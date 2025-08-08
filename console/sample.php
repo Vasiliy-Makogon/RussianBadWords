e87c25a973ecd58166a40b55c8fc8266f995cf44
Vasiliy <1030328+Vasiliy-Makogon@users.noreply.github.com>
2025-08-08 04:24:20 +0300

    Refactoring

--- console/sample.php
+++ console/sample.php
@@ -7,10 +7,17 @@ require __DIR__ . '/../../../autoload.php';
 // Проверка сообщения от пользователя.
 // В слове "электрo-фишер" кириллическая буква "о" заменена на латинскую,
 // а слова умышленно соединены различными символами с целью обмана программы.
-$message = 'Продам_электрo-фишер.fisher-f-3500 не дорого! Ну и немного нембутала';
+// В слове "сукa" кириллическая буква "a" заменена на латинскую.
+$message = 'Продам_электрo-фишер.fisher-f-3500 не дорого! Ну и немного нембутала, сукa';
 
 $validator = new Krugozor\RussianBadWords\Items\StopWordsValidator($message);
 if (!$validator->validate()) {
     echo "Текст не проходит валидацию! Плохие слова:\n";
+    print_r($validator->getFailedWords());
+}
+
+$validator = new Krugozor\RussianBadWords\Items\ProfanityWordsValidator($message);
+if (!$validator->validate()) {
+    echo "Текст не проходит валидацию! Ненормативная лексика:\n";
     print_r($validator->getFailedWords());
 }
